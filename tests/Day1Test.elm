18885e3837c14ca4f52322c56ff35a406fe24bd9
Fotis Papadogeorgopoulos <fotis@futurice.com>
2019-12-11 19:16:21 +0200

    Fix test

--- tests/Day1Test.elm
+++ tests/Day1Test.elm
@@ -5,16 +5,17 @@ import Expect exposing (Expectation)
 import Test exposing (..)
 
 
-suite : List Test
+suite : Test
 suite =
     -- Read https://package.elm-lang.org/packages/elm-explorations/test/latest for more information!
-    [ describe "Part 1"
-        [ test "Does the thing" <|
-            \_ ->
-                let
-                    solution =
-                        Day1.part1 "This is a fake input"
-                in
-                Expect.pass
+    describe "Day 1"
+        [ describe "Part 1"
+            [ test "Does the thing" <|
+                \_ ->
+                    let
+                        solution =
+                            Day1.part1 "This is a fake input"
+                    in
+                    Expect.pass
+            ]
         ]
-    ]
